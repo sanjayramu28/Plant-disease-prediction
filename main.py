6ce0c51dc2dfb993df1766ccf9dcc2ac00772858
Sanjay <98222052+sanjayramu28@users.noreply.github.com>
2025-03-18 22:07:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -58,8 +58,13 @@ async def predict(file: UploadFile = File(...)):
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))  # Use Render's port or default to 8000
+    uvicorn.run(app, host="0.0.0.0", port=port)
