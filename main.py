ac9ba3ea73fec8778856fcd4785f960c0dd35311
Sanjay <98222052+sanjayramu28@users.noreply.github.com>
2025-03-23 18:52:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ app.add_middleware(
 )
 
 if __name__ == "__main__":
-    import uvicorn
-    port = int(os.environ.get("PORT", 10000))  # Default to 10000 if PORT is not set
-    uvicorn.run(app, host="0.0.0.0", port=port)
+  import uvicorn
+  port = int(os.environ.get("PORT", 8000))  # Default to 8000 if PORT is not set
+  uvicorn.run(app, host="0.0.0.0", port=port)
 
